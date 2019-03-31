c90c440e5aeafb8aa1eb2fe5eae7a4fef348c67b
RAHEEL-AHMED-KHAN <48627331+RAHEEL-AHMED-KHAN@users.noreply.github.com>
2019-03-31 18:41:51 +0500

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,26 +1,27 @@
 #!/usr/bin/env bash 
 # File: guessinggame.sh 
 # Peer graded assignment for The Unix Workbench
-
-count=$(ls -1 | wc -l) 
  
-function guess_the_number {        
-    if [[ $1 -lt $2 ]]          
-    then 
- 	echo "NO..Less than the correct one!" 
-    elif [[ $1 -gt $2 ]] 
-    then 
- 	echo "NO..Higher than the correct one!" 
-    else 
-	echo "" 
- 	echo "YES... CORRECT - You Win!!!" 
-    fi 
+ echo "[Welcome to My Unix WorkBench Guessing game]" 
+ 
+ function RaiseQuery { 
+ 	echo "Please enter the number of files in the current directory:" 
+ 	read guess 
+    files=$(ls -1 | wc -l) 
  } 
+  
+ RaiseQuery
  
- 
- while [[ $count -ne $guess_number ]]    
+ while [[ $guess -ne $files ]] 
  do 
-     read -p "Please guess how many files are in the current directory? " guess_number 
-     echo $(guess_the_number $guess_number $count) 
-     echo "" 
+ 	if [[ $guess -lt $files ]]  
+ 	then 
+ 		echo "Too low for Guessing." 
+ 	else 
+ 		echo "Too high for Guessing" 
+ 	fi 
+ 	RaiseQuery
  done 
+ 
+ echo "Well done! It is the correct answer" 
+ 
